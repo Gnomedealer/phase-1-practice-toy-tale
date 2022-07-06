068376cefc4e79252220bd15b5082a5ea217a2b8
Tommy Nguyen <sixty22tommy@gmail.com>
2022-07-06 15:39:30 -0700

    working with jared

--- src/index.js
+++ src/index.js
@@ -5,24 +5,79 @@ document.addEventListener("DOMContentLoaded", () => {
     return fetch('http://localhost:3000/toys/')
     .then(dolls => dolls.json())
     .then(toys => makeDiv(toys))
-    
   }
+  const postToys = (name,imageUrl) => {}
+
+
+
+
+
+
+    fetch('http://localhost:3000/toys',
+    {
+      headers:{
+        'Content-type': 'application/json'
+         Accept: "application/json"
+      },
+      method: "POST"
+      body: JSON.stringify(
+        {
+        }
+      )
+
+
+
+    })
+    .then()
+    .then()
+  }
+
+  toyButton = document.getElemenyById('new-toy-btn')
+  toyButton.addEventListener('click', event => {
+
+
+  })
+
+
   function makeDiv(toys){
     const mainDiv = document.getElementById('toy-collection')
+
     toys.forEach(toy => {
-      const div = document.createElement('div')
+      const div = document.createElement('div');
+      const h2 = document.createElement('h2');
+      const img = document.createElement('img');
+      const p = document.createElement('p');
+      const btn = document.createElement('button');
+
+      btn.className = 'like-btn';
       div.className = 'card'
+      btn.setAttribute('id', toy.id);
+
+      h2.textContent = toy.name;
+      img.src = toy.image;
+
+      img.style.height = '250';
+      img.style.width = '250px';
+
+      p.textContent = `${toy.likes} likes`;
+      btn.textContent = 'like';
+      div.append(h2,img,p,btn);
       mainDiv.append(div)
     })
-
-    
-
-
-
-     
   }
-  getToys()
+
     
+
+
+
+
+
+
+
+
+
+
+
   const addBtn = document.querySelector("#new-toy-btn");
   const toyFormContainer = document.querySelector(".container");
   addBtn.addEventListener("click", () => {
@@ -34,5 +89,7 @@ document.addEventListener("DOMContentLoaded", () => {
       toyFormContainer.style.display = "none";
     }
   });
-});
 
+
+  getToys();
+});
