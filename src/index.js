7a563959aa9f0148ef181a895fe2cbbbc6f99fec
Tommy Nguyen <sixty22tommy@gmail.com>
2022-07-06 14:55:39 -0700

    working with Jared

--- src/index.js
+++ src/index.js
@@ -1,6 +1,28 @@
 let addToy = false;
 
 document.addEventListener("DOMContentLoaded", () => {
+  const getToys = () => {
+    return fetch('http://localhost:3000/toys/')
+    .then(dolls => dolls.json())
+    .then(toys => makeDiv(toys))
+    
+  }
+  function makeDiv(toys){
+    const mainDiv = document.getElementById('toy-collection')
+    toys.forEach(toy => {
+      const div = document.createElement('div')
+      div.className = 'card'
+      mainDiv.append(div)
+    })
+
+    
+
+
+
+     
+  }
+  getToys()
+    
   const addBtn = document.querySelector("#new-toy-btn");
   const toyFormContainer = document.querySelector(".container");
   addBtn.addEventListener("click", () => {
@@ -13,3 +35,4 @@ document.addEventListener("DOMContentLoaded", () => {
     }
   });
 });
+
